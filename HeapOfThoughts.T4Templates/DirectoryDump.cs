be25d2ba6aeabc2048d22171d7cdeba1161f7090
woro <wrozmiarek@gmail.com>
2014-07-16 00:43:19 +0200

    Adding DirectoryDump T4 template.

---
+++ HeapOfThoughts.T4Templates/DirectoryDump.cs
@@ -0,0 +1,13 @@
+﻿namespace Test 
+{
+	public static class Resources 
+	{ 
+		public static readonly string File1_txt = @"D:\Development\Repository\HeapOfThoughts\HeapOfThoughts.T4Templates\Resources\File1.txt";
+		
+		public static class Child 
+		{ 
+			public static readonly string File2_txt = @"D:\Development\Repository\HeapOfThoughts\HeapOfThoughts.T4Templates\Resources\Child\File2.txt";
+			
+		} 
+	} 
+}
